26bfaca1082a018160e06bae9f7c369e035f4507
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 22:14:45 +0530

    added

---
+++ AddressBook.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace b
+{
+    public class AddressBook
+    {
+        //List<ContactPerson> ls = new List<ContactPerson>();
+        string path = "F:\\file1.txt";
+
+
+
+
+
+
+        public void AddContact(string m1, string m2, string m3, string m4, string m5, string m6,string m7)
+        {
+            
+            string contactInfo = $"{m1},{m2},{m3},{m4},{m5},{m6},{m7}";
+            using (StreamWriter writer = File.AppendText(path))
+            {
+               
+               writer.WriteLine(contactInfo);
+            }
+
+            Console.WriteLine("Contact added successfully!");
+        }
+
+
+
+
+        public void EditContact(string firstName, string lastName)
+        {
+           
+            string[] lines = File.ReadAllLines(path);
+
+            
+            bool contactFound = false;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] s = lines[i].Split(',');
+                if (s[0].Equals(firstName) && s[1].Equals(lastName))
+                {
+                    Console.WriteLine($"Editing contact: {firstName} {lastName}");
+
+                   
+                    Console.Write("Enter new Address: ");
+                    string newAddress = Console.ReadLine();
+                    Console.Write("Enter new City: ");
+                    string newCity = Console.ReadLine();
+                    Console.Write("Enter new Zip: ");
+                    string newZip = Console.ReadLine();
+                    Console.Write("Enter new Phone Number: ");
+                    string newPhoneNumber = Console.ReadLine();
+                    Console.Write("Enter new Email: ");
+                    string newEmail = Console.ReadLine();
+
+                  
+                    lines[i] = $"{firstName},{lastName},{newAddress},{newCity},{newZip},{newPhoneNumber},{newEmail}";
+                    contactFound = true;
+                    Console.WriteLine("Contact updated successfully!");
+                    break;
+                }
+            }
+
+            if (!contactFound)
+            {
+                Console.WriteLine($"Contact with name {firstName} {lastName} not found.");
+            }
+
+            
+            File.WriteAllLines(path, lines);
+        }
+
+        public void DeleteContact(string firstName, string lastName)
+        {
+            
+            List<string> lines = new List<string>(File.ReadAllLines(path));
+
+            
+            bool contactFound = false;
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] contactInfo = lines[i].Split(',');
+                if (contactInfo[0].Equals(firstName) && contactInfo[1].Equals(lastName))
+                {
+
+                    Console.WriteLine($"Deleting contact: {firstName} {lastName}");
+
+
+                    lines.RemoveAt(i);
+                    contactFound = true;
+                    Console.WriteLine("Contact deleted successfully!");
+                    break;
+                }
+            }
+
+            if (!contactFound)
+            {
+                Console.WriteLine($"Contact with name {firstName} {lastName} not found.");
+            }
+
+            
+            File.WriteAllLines(path, lines);
+        }
+
+        public void DisplayContacts()
+        {
+            
+           string[] lines = File.ReadAllLines(path);
+            if(lines == null)
+            {
+                Console.WriteLine("empty addressbook!");
+            }
+            else
+            {
+                Console.WriteLine("Address Book Contacts:");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            
+
+
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+
+
+namespace b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook ab = new AddressBook();
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Delete Contact");
+                Console.WriteLine("4. Display Contacts");
+                Console.WriteLine("5. Exit");
+
+                
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("enter the data in this format Ex: name =Ramesh , last name = A,city = Mumbai , ZIP = 567-549 ,phonenumber = 9812346579, email =bangalore01@gmail.com ");
+                        string namePattern = "[A-Z]{1}[a-z]";
+                        string LnamePatter = "[A-Z]";
+                        string cityPattermn = "[A-Z]{1}[a-z]";
+                        string zipPattermn = "[0-9]{3}[-]{1}[0-9]{3}";
+                        string pnpattern = "[8,7,6,9]{1}[0-9]{9}";
+                        string mailpattern = "[a-z]{2,9}[0-9]{1,9}[@]{1}[a-z]{1,9}[.]{1}[a-z]{2,5}";
+
+                        Console.Write("Enter First Name: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Enter Last Name: ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Enter Address: ");
+                        string address = Console.ReadLine();
+                        Console.Write("Enter City: ");
+                        string city = Console.ReadLine();
+                        Console.Write("Enter Zip: ");
+                        string zip = Console.ReadLine();
+                        Console.Write("Enter Phone Number: ");
+                        string phoneNumber = Console.ReadLine();
+                        Console.Write("Enter Email: ");
+                        string email = Console.ReadLine();
+
+
+
+                        bool isName = Regex.IsMatch(firstName, namePattern);
+                        bool isLname = Regex.IsMatch(lastName, LnamePatter);
+                        bool isCity = Regex.IsMatch(city, cityPattermn);
+                        bool iszip = Regex.IsMatch(zip, zipPattermn);
+                        bool ispn = Regex.IsMatch(phoneNumber, pnpattern);
+                        bool ismail = Regex.IsMatch(email, mailpattern);
+
+                        Console.WriteLine(isName);
+                        Console.WriteLine(isLname);
+                        Console.WriteLine(isCity);
+                        Console.WriteLine(iszip);
+                        Console.WriteLine(ispn);
+                        Console.WriteLine(ismail);
+                        string a1 = null;
+                        string a2 = null;
+                        string b1 = null;
+                        string b2 = null;
+                        string b3 = null;
+                        string b4 = null;
+
+                        try
+                        {
+                            if(isName && isLname && isCity && iszip && ispn && ismail)
+                            {
+                                a1=firstName;
+                                a2=lastName;
+                                b1=city;
+                                b2=zip;
+                                b3 = phoneNumber;
+                                b4 = email;
+
+                               
+                               // ContactPerson newContact = new ContactPerson(a1, a2, address, b1, b2, b3, b4);
+
+                                ab.AddContact(a1, a2, address, b1, b2, b3, b4);
+                            }
+                            else
+                            {
+                                throw new invalidInput("exception has occured ..please check your input data");
+                                //Console.WriteLine("enter valid format (Ex: city = Mumbai , ZIP = 567-549 ,phonenumber = 9812346579, email =bangalore01@gmail.com )");
+                                
+                            }
+                        }
+                        catch (invalidInput e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+
+                        
+                        break;
+
+                    case 2:
+                        
+                        Console.Write("Enter First Name of Contact to Edit: ");
+                        string editFirstName = Console.ReadLine();
+                        Console.Write("Enter Last Name of Contact to Edit: ");
+                        string editLastName = Console.ReadLine();
+
+                        ab.EditContact(editFirstName, editLastName);
+                        break;
+
+                    case 3:
+                        Console.Write("Enter First Name of Contact to Delete: ");
+                        string deleteFirstName = Console.ReadLine();
+                        Console.Write("Enter Last Name of Contact to Delete: ");
+                        string deleteLastName = Console.ReadLine();
+
+                        ab.DeleteContact(deleteFirstName, deleteLastName);
+                        break;
+
+                    case 4:
+                        ab.DisplayContacts();
+                        break;
+
+                    case 5:
+                        
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter the valid choice.");
+                        break;
+                }
+
+
+
+            }
+        }
+
+    }
+}
